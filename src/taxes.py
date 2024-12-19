60ea6e409af668fff0433adbc7aae7aef7dbc288
Sergey Minin <sm.minins@gmail.com>
2024-12-19 20:45:56 +0300

    add tnd test calculate_tax()

--- src/taxes.py
+++ src/taxes.py
@@ -13,3 +13,14 @@ def calculate_taxes(prices: list[float], tax_rate: float) -> list[float]:
         taxed_prices.append(price + tax)
 
     return taxed_prices
+
+
+def calculate_tax(price: float, tax_rate: float) -> float:
+    '''Вычисляет стоимость товара с учетом налога и возвращать результат'''
+
+    if price < 0:
+        raise ValueError('Неверная цена')
+    if tax_rate < 0 or tax_rate >= 100:
+        raise ValueError('Неверный налоговый процент')
+    result = price * (100.0 + tax_rate) / 100.0
+    return result
--- tests/test_taxes.py
+++ tests/test_taxes.py
@@ -1,5 +1,5 @@
 import pytest
-from src.taxes import calculate_taxes
+from src.taxes import calculate_taxes, calculate_tax
 
 @pytest.fixture
 def prices():
@@ -24,3 +24,32 @@ def test_calculate_taxes_invalid_tax_rate(prices):
 def test_calculate_taxes_invalid_tax_prices():
     with pytest.raises(ValueError):
         calculate_taxes([0, -1], tax_rate = 10)
+
+
+@pytest.mark.parametrize("price, tax_rate, expected", [
+    ([100.0, 10.0, 110.0]),
+    ([50.0, 5.0, 52.5])
+])
+
+def test_calculate_tax(price, tax_rate, expected):
+    assert calculate_tax(price, tax_rate) == expected
+
+
+def test_calculate_tax_price_minus():
+    with pytest.raises(ValueError):
+        calculate_tax(-0.1, tax_rate = 10)
+
+
+def test_calculate_tax_tax_rate_incorrect():
+    with pytest.raises(ValueError):
+        calculate_tax(100.0, -0.1)
+
+def test_calculate_tax_tax_rate_incorrect_2():
+    with pytest.raises(ValueError):
+        calculate_tax(100.0, 100.0)
+
+def test_calculate_tax_tax_rate_incorrect_3():
+    with pytest.raises(ValueError):
+        calculate_tax(100.0, 100.1)
+
+
